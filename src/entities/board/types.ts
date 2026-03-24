223104b1fef346a7a866f9b0edaa4e495475fc95
kuman514 <hadjadj0@gmail.com>
2026-03-25 03:56:26 +0900

    refactor: purge shared types & add next move type to board

--- src/entities/board/types.ts
+++ src/entities/board/types.ts
@@ -1,4 +1,10 @@
-import { NextMove } from '^/shared/types';
+/**
+ * @desc
+ * 말이 움직일 때의 목적지의 모든 유형의 집합.
+ * `'finish'`: 마지막 칸같이 움직이면 골인이 되는 목적지.
+ * 그 외(숫자): 한 칸씩 갈 때 가야 하는 목적지.
+ */
+export type NextMove = number | 'finish';
 
 export interface BoardNodeRoute {
   /**
